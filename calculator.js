0fa183efb41de86b1abd51b5abfc849a5f5dccfd
Abel Haddis (Student) <ahaddis@sundevils.asu.edu>
2022-08-18 11:58:22 -0700

    Lots of progress on calculator functionality
    The user can now clear their screen, clear a number or operator, and only enter one decimal on a number to avoid errors> I will add a +/- button to handle negative integers, work on keyboard input for operations, and display the results better.

--- calculator.js
+++ calculator.js
@@ -5,9 +5,7 @@ const calc_btns = document.querySelectorAll("button");
 const equals_btn = document.querySelector(".equals");
 const clear_all_btn = document.querySelector(".clear_all");
 const backsapce_btn = document.querySelector(".clear");
-
-let number1 = 0;
-let number2 = 0;
+const decimal_btn = document.querySelector(".decimal");
 let operators = "";
 let operands = "";
 let operationChain = "";
@@ -18,28 +16,53 @@ calc_btns.forEach(btn => {
         operationText.textContent = operationChain;
         //Array that will contain only the operator symbols
         operators = operationChain.replace(/[0-9.]/g, "").split("");
-        console.log("Operators: " + operators);
-        console.log("Operands: " + operands);
         //Array that will only contain the numbers being operated on
-        operands = operationChain.split(/[\+\/\*\-%]/g).filter(n => !!n); //Checking if string has length > 0 then splitting it on operators   
+        operands = operationChain.split(/[\+\/\*\-%]/g).filter(n => !!n); //Checking if string has length > 0 then splitting it on operators 
     });
 });
 
+decimal_btn.addEventListener("click", () => {   
+    //Check if user has not used the decimal for the current number
+    if(!operands[operands.length-1].includes("."))
+    {
+        operationChain += decimal_btn.innerText;  
+        //Update array so an attempt at entering duplicate decimals can be catched
+        //when event fires again 
+        operands = operationChain.split(/[\+\/\*\-%]/g).filter(n => !!n);        
+    }
+    else
+    {
+        operationChain+=""; //Add nothing if user has already used decimal
+    }
+    operationText.textContent = operationChain;
+});
+
 equals_btn.addEventListener("click", () => {
-    if(operators.length >= operands.length)  //Ensure user enters correct operation
+    if(operators.length >= operands.length)  //Ensure user doesn't end operation chain with an operator
     {
         result.textContent = "Math Error!";
+    }
+    else if(operators.length === 0 && operands.length === 1)  //Only one digit is entered
+    {
+        result.textContent = operands[0];
     }
     else
     {
         //While loop ensures that an operation chain is performed with two numbers at a time
         //until the operand array has one element remaining (the final result)
-        while(operands.length !== 1)
+        while(operands.length > 1)
         {
-            operands[0] = operate(+operands[0], +operands[1], operators[0]);
+            operands[0] = Math.round(operate(+operands[0], +operands[1], operators[0]) * 1000000000) / 1000000000;
             operands.splice(1,1); //Remove the second element from array
-            operators.shift();
-            result.textContent = `${operands[0]}`;
+            operators.shift(); 
+            if(isNaN(operands[0]))
+            {
+                result.textContent = "You can't do that dummy!";
+            }
+            else
+            {
+                result.textContent = `${operands[0]}`;
+            }
         }
     }
 });
@@ -50,6 +73,14 @@ clear_all_btn.addEventListener("click", () => {
     operationChain = "";
     result.textContent = "0";
     operationText.textContent = "";
+});
+
+backsapce_btn.addEventListener("click", () => {
+    //Remove last char and update both arrays to make sure the correct element is removed
+    operationChain = operationChain.slice(0, -1); //Remove last char 
+    operators = operationChain.replace(/[0-9.]/g, "").split("");
+    operands = operationChain.split(/[\+\/\*\-%]/g).filter(n => !!n); 
+    operationText.textContent = operationChain;
 });
 
 function operate(num1, num2, operator)
@@ -65,7 +96,7 @@ function operate(num1, num2, operator)
     }
     else if(operator === "/")
     {
-        return Math.round(divide(num1, num2) * 1000000000) / 1000000000;
+        return divide(num1, num2);
     }
     else if(operator === "*")
     {
@@ -73,7 +104,7 @@ function operate(num1, num2, operator)
     }
     else if(operator === "%")
     {
-        return Math.round(modulous(num1, num2) * 1000000000) / 1000000000;
+        return modulous(num1, num2);
     }
     else
     {
@@ -100,7 +131,7 @@ function divide(num1, num2)
 {   
     if(num2 === 0)
     {
-        return result.textContent = "Can't divide by 0 dummy!";
+        return "undefined";
     }
     return num1/num2
 };
@@ -109,7 +140,7 @@ function modulous(num1, num2)
 {
     if (num2 === 0)
     {
-        return result.textContent = "Can't mod by 0 dummy!";
+        return "undefined";
     }
     return num1%num2;
 }
